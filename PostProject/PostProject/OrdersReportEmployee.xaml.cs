53b85a5cd0fe2209662096600e16e522b2f3c37c
Amir <Amirgmali.com@gmail.com>
2023-06-26 22:56:44 +0330

    New buttoms added

---
+++ PostProject/PostProject/OrdersReportEmployee.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for OrdersReportEmployee.xaml
+    /// </summary>
+    public partial class OrdersReportEmployee : UserControl
+    {
+        public OrdersReportEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void Serach_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
