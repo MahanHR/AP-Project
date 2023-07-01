5c2ef416860260f92b7c435d341f6ac730bf61fe
Amir <Amirgmali.com@gmail.com>
2023-07-01 22:28:27 +0330

    Full Edit

---
+++ PostProject/PostProject/SendEmailWin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for SendEmailWin.xaml
+    /// </summary>
+    public partial class SendEmailWin : UserControl
+    {
+        public SendEmailWin()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ShowInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PostProject/PostProject/SubmitOrderWin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for SubmitOrderWin.xaml
+    /// </summary>
+    public partial class SubmitOrderWin : UserControl
+    {
+        public SubmitOrderWin()
+        {
+            InitializeComponent();
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
