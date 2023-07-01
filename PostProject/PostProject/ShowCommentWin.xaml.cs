51bcf1d9459abdc6b47dcad26bd8cdcaf410b697
Amir <Amirgmali.com@gmail.com>
2023-07-01 22:28:27 +0330

    Full Edit

---
+++ PostProject/PostProject/ShowCommentWin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for ShowCommentWin.xaml
+    /// </summary>
+    public partial class ShowCommentWin : UserControl
+    {
+        public ShowCommentWin()
+        {
+            InitializeComponent();
+        }
+        private void ShowOrders_Click(object sender, RoutedEventArgs e)
+        {
+            Orders.Text = "";
+        }
+    }
+}
