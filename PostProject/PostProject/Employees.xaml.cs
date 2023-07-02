c0d1170336037c381909b2038d2b428d42889afe
Amir <Amirgmali.com@gmail.com>
2023-07-02 13:28:09 +0330

    Serviced2

--- PostProject/PostProject/Employees.xaml.cs
+++ PostProject/PostProject/Employees.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PostProject
 {
@@ -50,12 +38,14 @@ namespace PostProject
 
         private void ShowComments_Click(object sender, RoutedEventArgs e)
         {
-            ButtonShow.Content = new ShowComment();
+            ButtonShow.Content = new ShowCommentWin();
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.MainWindow.Content = new Login();
+            var p = new MainWindow();
+            p.Show();
+            this.Close();
         }
 
         private void HomeB_Click(object sender, RoutedEventArgs e)
