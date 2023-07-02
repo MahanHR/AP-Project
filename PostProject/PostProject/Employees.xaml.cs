250bc4882e2f4dba9ea4346e66214e90c26b7a9e
Mahan Heidari <mahanheidari.hr@gmail.com>
2023-07-02 13:54:01 +0330

    Complete Replace

---
+++ PostProject/PostProject/Employees.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for Employees.xaml
+    /// </summary>
+    public partial class Employees : Window
+    {
+        public Employees()
+        {
+            InitializeComponent();
+        }
+        private void RegisterCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new RegisterCustomerWin();
+        }
+
+        private void SubmitOrder_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new SubmitOrderWin();
+        }
+
+        private void OrdersReport_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new OrderReportEmpWin();
+        }
+
+        private void PackageInfo_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new PackageInfoEmpWin();
+        }
+
+        private void SendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new SendEmailWin();
+        }
+
+        private void ShowComments_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new ShowCommentWin();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            var p = new MainWindow();
+            p.Show();
+            this.Close();
+        }
+
+        private void HomeB_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonShow.Content = new EmptyWin();
+        }
+    }
+}
---
+++ PostProject/PostProject/OrderReportWin.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace PostProject
+{
+    /// <summary>
+    /// Interaction logic for OrderReportWin.xaml
+    /// </summary>
+    public partial class OrderReportWin : UserControl
+    {
+        public OrderReportWin()
+        {
+            InitializeComponent();
+        }
+        private void ShowOrders_Click(object sender, RoutedEventArgs e)
+        {
+            Orders.Text = "";
+            string SSN = "", Sending = "";
+            try
+            {
+                SqlConnection conn = new(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\source\repos\HajAmir-Post\AP-Project\PostProject\PostProject\SQL\save.mdf;Integrated Security=True");
+                conn.Open();
+                string command = "select * from Customer where isEntered = '" + 2 + "'";
+                SqlDataAdapter adapter = new(command, conn);
+                DataTable data2 = new();
+                adapter.Fill(data2);
+                SSN = data2.Rows[0][4].ToString();
+                string command2 = "select * from Orders";
+                adapter = new(command2, conn);
+                DataTable data = new();
+                adapter.Fill(data);
+                int found = 0;
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    if(SSN == data.Rows[i][4].ToString())
+                    {
+                        found++;
+                        string Ty = "", PoTy = "";
+                        if (int.Parse(data.Rows[i][3].ToString()) == 1)
+                        {
+                            Ty = "Object";
+                        }
+                        else if (int.Parse(data.Rows[i][3].ToString()) == 2)
+                        {
+                            Ty = "Document";
+                        }
+                        else if (int.Parse(data.Rows[i][3].ToString()) == 3) 
+                        {
+                            Ty = "Fragile";
+                        }
+                        if (int.Parse(data.Rows[i][5].ToString()) == 1)
+                        {
+                            PoTy = "Ordinary";
+                        }
+                        else if (int.Parse(data.Rows[i][5].ToString()) == 2)
+                        {
+                            PoTy = "Speed";
+                        }
+                        Sending += found + "." + "ID : " + data.Rows[i][0].ToString() + "   Origin : " + data.Rows[i][1].ToString() + "   Destination : " + data.Rows[i][2].ToString() + "   Type : " + Ty + "\nPost Type : " + PoTy + "   Is Expensive? " + data.Rows[i][6].ToString() + "   Is Received? " + data.Rows[i][10].ToString() + "   Weight : " + data.Rows[i][7].ToString() + "\nPrice : " + data.Rows[i][9].ToString() + "\n";
+                    }
+                }
+                if(found == 0)
+                {
+                    throw new Exception("You have not submitted any order");
+                }
+                conn.Close();
+                Sending = Sending + Sending + Sending + Sending + Sending + Sending + Sending + Sending + Sending + Sending ;
+                throw new Exception(Sending);
+            }
+            catch (Exception ex)
+            {
+                Orders.Text = ex.Message;
+            }
+        }
+
+        private void Orders_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+    }
+}
