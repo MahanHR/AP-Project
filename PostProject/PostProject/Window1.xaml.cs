15c782b34220c0f9de2a844c8565d88449cc6a0b
Mahan Heidari <mahanheidari.hr@gmail.com>
2023-07-02 14:13:18 +0330

    Regex part 1

--- PostProject/PostProject/Window1.xaml.cs
+++ PostProject/PostProject/Window1.xaml.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.Data.SqlClient;
-using System.Data;
 
 namespace PostProject
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window
     {
         public Window1()
         {
             InitializeComponent();
         }
+
         public void TextBox_Name(object sender, TextChangedEventArgs e)
         {
 
@@ -34,14 +23,17 @@ namespace PostProject
         {
 
         }
+
         public void PasswordBox_Confirm(object sender, RoutedEventArgs e)
         {
 
         }
+
         public void TextBox_Email(object sender, TextChangedEventArgs e)
         {
 
         }
+
         public void TextBox_PersonnelID(object sender, TextChangedEventArgs e)
         {
 
@@ -51,10 +43,12 @@ namespace PostProject
         {
 
         }
+
         public void PasswordBox_Pass(object sender, RoutedEventArgs e)
         {
 
         }
+
         public void Login_Click(object sender, RoutedEventArgs e)
         {
             var p = new MainWindow();
@@ -66,6 +60,40 @@ namespace PostProject
         {
             try
             {
+                Regex firstNameRegex = new(@"^[a-zA-Z]{3,32}$");
+                Regex lastNameRegex = new(@"^[a-zA-Z]{3,32}$");
+                Regex personnelIDRegex = new(@"^\d{2}9\d{2}$");
+                Regex usernameRegex = new(@"^\S{3,32}$");
+                Regex passwordRegex = new(@"^(?=\S{8})(?!\S{33})(\S*[a-z]+\S*[A-Z]+\S*|\S*[A-Z]+\S*[a-z]+\S*)$");
+                Regex emailRegex = new(@"^[\w-\.]{3,32}@[A-z]{3,32}.[A-z]{2,3}$");
+                if (!firstNameRegex.Match(Name.Text.ToString()).Success)
+                {
+                    throw new Exception("Input format of first name is not correct.");
+                }
+                if (!lastNameRegex.Match(LName.Text.ToString()).Success)
+                {
+                    throw new Exception("Input format of last name is not correct.");
+                }
+                if (!personnelIDRegex.Match(PersonnelID.Text.ToString()).Success)
+                {
+                    throw new Exception("Input format of personnel ID is not correct.");
+                }
+                if (!usernameRegex.Match(UName.Text.ToString()).Success)
+                {
+                    throw new Exception("Input format of username is not correct.");
+                }
+                if (!passwordRegex.Match(Pass.Password.ToString()).Success)
+                {
+                    throw new Exception("Input format of password is not correct.");
+                }
+                if (!passwordRegex.Match(PassCon.Password.ToString()).Success)
+                {
+                    throw new Exception("Input format of password is not correct.");
+                }
+                if (!emailRegex.Match(Email.Text.ToString()).Success)
+                {
+                    throw new Exception("Input format of email is not correct.");
+                }
                 SqlConnection conn = new(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\source\repos\HajAmir-Post\AP-Project\PostProject\PostProject\SQL\save.mdf;Integrated Security=True");
                 conn.Open();
                 string command = "select * from Employee";
@@ -74,11 +102,11 @@ namespace PostProject
                 adapter.Fill(data);
                 for (int i = 0; i < data.Rows.Count; i++)
                 {
-                    if (data.Rows[i][4] == UName.Text)
+                    if ((string)data.Rows[i][4] == UName.Text)
                     {
                         throw new Exception("The username is already used");
                     }
-                    if (data.Rows[i][3] == Email.Text)
+                    if ((string)data.Rows[i][3] == Email.Text)
                     {
                         throw new Exception("The email is already used");
                     }
